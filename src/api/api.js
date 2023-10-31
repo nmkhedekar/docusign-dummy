f7cc7bed394de3d64ff451906fcf66e14faf2073
nmkhedekar <nmkhedekar@virtualemployee.com>
2023-10-31 18:59:56 +0530

    docusign-dummy

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const accessToken = localStorage.getItem("accessToken");
 export const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "http://54.209.82.1:5000",//"http://localhost:5000",
 });
 
 // if(accessToken){
